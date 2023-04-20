f56710a1855178a19b6f91f120a08e499e315c73
tahirikosan <tahirabuzetoglu@gmail.com>
2023-04-21 01:02:40 +0300

    Oyuncu Kuyruk

---
+++ Assets/Scripts/TailController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TailController : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D connectedBody;
+    private HingeJoint2D hingeJoint2D;
+
+    public void Setup(Rigidbody2D rigidbody2D)
+    {
+        connectedBody = rigidbody2D;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hingeJoint2D = GetComponent<HingeJoint2D>();
+        hingeJoint2D.connectedBody = connectedBody;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
