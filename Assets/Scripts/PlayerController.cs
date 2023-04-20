bcab6b6e9a94fe524e6df50d5350c3acd465227d
tahirikosan <tahirabuzetoglu@gmail.com>
2023-04-20 15:15:27 +0300

    Karakter can bar

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,6 +11,12 @@ public class PlayerController : MonoBehaviour
     FixedJoystick fixedJoystick;
 
     Vector2 moveVector;
+
+    private float MAX_HP = 100;
+    private float currentHp = 100;
+
+    [SerializeField]
+    private Image imgHp;
 
     // Start is called before the first frame update
     void Start()
@@ -44,5 +51,28 @@ public class PlayerController : MonoBehaviour
         float zAxis = Mathf.Atan2(hAxis, vAxis) * Mathf.Rad2Deg;
 
         transform.eulerAngles = new Vector3(0, 0, -zAxis);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("enemy_bullet"))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    private void TakeDamage(int damage)
+    {
+        currentHp -= damage;
+        imgHp.fillAmount = currentHp / MAX_HP;
+        if (currentHp <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+        // Restart game
     }
 }
